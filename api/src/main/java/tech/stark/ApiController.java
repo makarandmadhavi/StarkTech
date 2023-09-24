c6fc0da91e29b9907b249e3e9256452fe4cf7507
Makarand <madhavi.m@northeastern.edu>
2023-09-24 03:47:17 -0400

    Add micronaut api

---
+++ api/src/main/java/tech/stark/ApiController.java
@@ -0,0 +1,12 @@
+package tech.stark;
+
+import io.micronaut.http.annotation.*;
+
+@Controller("/api")
+public class ApiController {
+
+    @Get(uri="/", produces="text/plain")
+    public String index() {
+        return "Example Response";
+    }
+}
